054cda5c02e6b3134f48d10e631ee46588488342
buh-h <maxwellwang22@gmail.com>
2024-08-31 16:39:04 -0500

    Fixed issues with MINE_VALUE and FLAG_VALUE

--- test.cpp
+++ test.cpp
@@ -1,24 +1,13 @@
 #include <iostream>
-#include "board.hpp"
+#include "solver.hpp"
 #include <string>
 int main() {
 
     Board b(15);
-    b.click(0,0);
-    for (int i=0; i<b.fullBoard.size(); i++) {
-        for (int j=0; j<b.fullBoard.size(); j++) {
-            std::cout << b.fullBoard[i][j] << "\t";
-        }
-        std::cout << "\n";
-    }
+    Solver s(b);
+    s.guaranteedClick();
+    do {
 
-        std::cout << "\n";
-
-    for (int i=0; i<b.visibleBoard.size(); i++) {
-        for (int j=0; j<b.visibleBoard.size(); j++) {
-            std::cout << b.visibleBoard[i][j] << "\t";
-        }
-        std::cout << "\n";
-    }
+    } while (true);
     return 0;
 }
